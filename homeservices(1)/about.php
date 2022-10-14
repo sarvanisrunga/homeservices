70ed0495de121572ed71e59f586b84ed5a8ad245
sarvanisrunga <100275994+sarvanisrunga@users.noreply.github.com>
2022-10-14 10:38:46 +0530

    Add files via upload

---
+++ homeservices(1)/about.php
@@ -0,0 +1,34 @@
+<?php include_once "./include/header.php"; ?>
+
+<div class="container">
+    <div class="row">
+        <!-- <img src="images/bg1.jpg" class="col-4 card-img-top" alt="Logo" > -->
+
+        <div class="container col-8" style="margin-top: 50px;">
+            <div class="card">
+                <div class="card-header text-center">
+                    <strong>ANDHRA UNIVERSITY</strong>
+                </div>
+
+                <div class="card-body">
+
+                <strong class="card-title">Project Members</strong>
+                <ul >
+                    <li>Kaivalya Kumar Y</li>
+                    <li>Sarvani S</li>
+                    <li>Yaswanth P</li>
+                    <li>Soumya P</li>
+                </ul>
+            </div>
+
+            <div class="card-footer text-center">
+                <p>Project-Guide 1: Mohan Sir</p>
+                <p style="margin-top:-20px;">Lab-Guide 2: Janardhan Sir</p> 
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include_once "./include/footer.php"; ?>
---
+++ homeservices(1)/include/footer1.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+    <footer
+
+    style="border-top: 1px solid #333;width: 100%; position: scroll; bottom: 0px;text-align: center;height: 40px; line-height: 40px;background: #353a3f;margin-top:50px; ">
+    Home Services &copy; <?= date("Y") ?>
+
+</footer>
+</body>
+
+</html>
